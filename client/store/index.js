95913b9d25605e76f83da8755a7ee7ddace072ce
Ray Nimalan <raymondnimalan@gmail.com>
2021-05-26 18:25:15 -0400

    feat: added redux logging

--- client/store/index.js
+++ client/store/index.js
@@ -1,11 +1,14 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import {createLogger} from 'redux-logger'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import photoReducer from './photo'
 
 const reducer = combineReducers({photoReducer})
 
-const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware))
+const middleware = composeWithDevTools(
+  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
+)
 const store = createStore(reducer, middleware)
 
 export default store
