b7465da7490ef103936b79578e0e6aa05574230a
Ray Nimalan <raymondnimalan@gmail.com>
2021-05-27 14:06:45 -0400

    feat: added mask component

--- client/app.js
+++ client/app.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './components/Home'
 import WebcamCapture from './components/WebcamCapture'
+import Mask from './components/Mask'
 
 const App = () => {
   return (
     <div>
       <Home />
       <WebcamCapture />
+      <Mask />
     </div>
   )
 }
---
+++ client/components/Mask.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Mask = () => {
+  return <div />
+}
+
+export default Mask
