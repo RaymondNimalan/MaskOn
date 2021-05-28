b1c227f02576839ea97830080e70688265eb996b
Ray Nimalan <raymondnimalan@gmail.com>
2021-05-28 16:47:28 -0400

    feat: setting up toggle feature and general styling

--- client/components/About.js
+++ client/components/About.js
@@ -1,26 +1,29 @@
 import React from 'react'
+import {Content, HowItWorks, Instructions} from './styled/AboutStyles'
+import {FiEyeOff, FiEye} from 'react-icons/fi'
 
 const About = () => {
   return (
-    <div>
-      <h1>How It Works!</h1>
-      <p>
-        This application utilizes the Google Vision API which can detect
-        different objects within an image. A list of labels is sent back and
-        verified to contain the labels 'mask' or 'personal protective
-        equipment'.
-      </p>
-
-      <h1>Instructions:</h1>
-      <ol>
-        <li>1. Allow browser to access webcam.</li>
-        <li>2. Take a screenshot using the camera button.</li>
-        <li>
+    <Content>
+      <HowItWorks>
+        <h1>How It Works!</h1> <FiEyeOff />
+        <p>
+          This application utilizes the Google Vision API which employs object
+          detection through machine learning. A list of detected labels is
+          recieved and verified to contain the labels 'mask' or 'personal
+          protective equipment'.
+        </p>
+      </HowItWorks>
+      <Instructions>
+        <h1>Instructions:</h1> <FiEye />
+        <p>1. Allow browser to access webcam.</p>
+        <p>2. Take a screenshot using the camera button.</p>
+        <p>
           3. Send screenshot to the Google Vision API using the upload button.
-        </li>
-        <li>4. Check the results!</li>
-      </ol>
-    </div>
+        </p>
+        <p>4. Check the results!</p>
+      </Instructions>
+    </Content>
   )
 }
 
---
+++ client/components/styled/AboutStyles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+`
+
+export const HowItWorks = styled.div`
+  padding-bottom: 16px;
+`
+export const Instructions = styled.div`
+  padding-bottom: 16px;
+`
+
+// export const CameraContainer = styled.div``
+// export const CameraContainer = styled.div``
+// export const CameraContainer = styled.div``
