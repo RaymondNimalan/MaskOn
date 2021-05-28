0756c982552207900b62359cd9339510c8504e03
Ray Nimalan <raymondnimalan@gmail.com>
2021-05-28 18:27:27 -0400

    feat: about comp styling

--- client/components/styled/AboutStyles.js
+++ client/components/styled/AboutStyles.js
@@ -4,25 +4,42 @@ import {FiEyeOff, FiEye} from 'react-icons/fi'
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 1rem;
+  padding: 8px;
 `
 
 export const HowItWorks = styled.div`
+  display: flex;
+  flex-direction: column;
   padding-bottom: 16px;
+  width: 100%;
+  h1 {
+    display: flex;
+    font-size: 24px;
+    align-items: center;
+  }
 `
-export const Instructions = styled.div`
-  padding-bottom: 16px;
+export const Instructions = styled(HowItWorks)``
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `
 
-export const Header = styled.div``
-
-export const Icon = styled.div``
+export const Icon = styled.div`
+  display: flex;
+  padding: 8px;
+  align-items: center;
+  align-self: flex-end;
+`
 
 export const OpenIcon = styled(FiEye)`
+  display: flex;
   cursor: pointer;
   font-size: 32px;
 `
 export const CloseIcon = styled(FiEyeOff)`
+  display: flex;
   cursor: pointer;
   font-size: 32px;
 `
