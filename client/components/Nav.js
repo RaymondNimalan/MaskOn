d18d90a9d1080e8ccb2a2b3c9174f29f0692812c
Ray Nimalan <raymondnimalan@gmail.com>
2021-05-28 13:37:09 -0400

    feat: navbar styling

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,8 +1,21 @@
 import React from 'react'
-import {Navbar} from './styled/NavStyles'
+import {HiDotsVertical} from 'react-icons/hi'
+import {RiSurgicalMaskLine} from 'react-icons/ri'
+import {Navbar, Icon, NavMenu} from './styled/NavStyles'
 
 const Nav = () => {
-  return <Navbar />
+  return (
+    <Navbar>
+      <NavMenu>
+        <Icon>
+          <RiSurgicalMaskLine />
+        </Icon>
+        <Icon>
+          <HiDotsVertical />
+        </Icon>
+      </NavMenu>
+    </Navbar>
+  )
 }
 
 export default Nav
--- client/components/styled/NavStyles.js
+++ client/components/styled/NavStyles.js
@@ -1,13 +1,30 @@
 import styled from 'styled-components'
 
 export const Navbar = styled.nav`
-  height: 80px;
+  display: flex;
+  height: 60px;
   width: 100%;
   position: fixed;
   z-index: 100;
   background: black;
+  align-items: center;
+  flex-direction: row;
+`
+
+export const NavMenu = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`
+
+export const Icon = styled.div`
+  display: flex;
+  padding: 0 8px 0 8px;
+  color: white;
+  font-size: 48px;
 `
 
 // export const CameraContainer = styled.div``
-
+// export const CameraContainer = styled.div``
+// export const CameraContainer = styled.div``
 // export const CameraContainer = styled.div``
