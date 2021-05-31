d34409eee1a57f0ca1d35c0ab47108bea9626785
Ray Nimalan <raymondnimalan@gmail.com>
2021-05-31 16:52:20 -0400

    feat: functional loading screen with updated redux

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import {MainContainer} from './styled/HomeStyles'
 import SideBar from './SideBar'
 import WebcamCapture from './WebcamCapture'
+import LoadingSet from './StorySets/LoadingSet'
+import YesMask from './StorySets/YesMask'
+import NoMask from './StorySets/NoMask'
 
 const Home = () => {
+  const [mask, setMask] = useState(false)
+
+  //const dispatch = useDispatch()
+
+  const {labels, loading} = useSelector(state => state.photoReducer)
+
   return (
     <MainContainer>
       <SideBar />
-      <WebcamCapture />
+      {!loading ? <WebcamCapture /> : <LoadingSet />}
+
+      {/* {mask ? <NoMask /> : <YesMask />}
+      <LoadingSet /> */}
     </MainContainer>
   )
 }
--- client/store/photo.js
+++ client/store/photo.js
@@ -1,8 +1,22 @@
 import axios from 'axios'
 
+// INITIAL STATE
+
+const intialState = {
+  labels: [],
+  mask: null,
+  loading: false
+}
+
 // ACTION TYPE
 
 export const ANALYSIS = 'ANALYSIS'
+
+export const MASK_TRUE = 'MASK_TRUE'
+
+export const MASK_FALSE = 'MASK_FALSE'
+
+export const LOADING = 'LOADING'
 
 // ACTION CREATOR
 
@@ -11,25 +25,44 @@ export const analysis = data => ({
   data
 })
 
+export const maskOn = () => ({
+  type: MASK_TRUE
+})
+
+export const maskOff = () => ({
+  type: MASK_FALSE
+})
+
+export const loading = () => ({
+  type: LOADING
+})
+
 // THUNK
 
 export const _analysis = image => async dispatch => {
   try {
     const {data} = await axios.post('/api/', image)
+    dispatch(loading())
     dispatch(analysis(data))
+    setTimeout(() => {
+      dispatch(loading())
+    }, 3000)
+    // if (intialState.labels.includes('Beard')) {
+    //   dispatch(maskOn())
+    // } else {
+    //   dispatch(maskOff())
+    // }
   } catch (error) {
     console.log(error)
   }
-}
-
-const intialState = {
-  labels: []
 }
 
 const photoReducer = (state = intialState, action) => {
   switch (action.type) {
     case ANALYSIS:
       return {...state, labels: action.data}
+    case LOADING:
+      return {...state, loading: !state.loading}
     default:
       return state
   }
