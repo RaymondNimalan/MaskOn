7059db1cd3c74e8ba0f7b1683be3009bee67c18a
Ray Nimalan <raymondnimalan@gmail.com>
2021-05-31 16:50:49 -0400

    fix: added limit to body parsing middleware to prevent post size limit

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const createApp = () => {
   app.use(morgan('dev'))
 
   // body parsing middleware
-  app.use(express.json())
+  app.use(express.json({limit: '50mb'}))
   app.use(express.urlencoded({extended: true}))
 
   // compression middleware
