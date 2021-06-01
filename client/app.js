1977a2edb736b4110ff83f6311b1762345734c9b
Ray Nimalan <raymondnimalan@gmail.com>
2021-06-01 16:45:36 -0400

    feat: app.js now can toggle between home component and loading component

--- client/app.js
+++ client/app.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 //import Home from './components/Home'
 import WebcamCapture from './components/WebcamCapture'
 import Mask from './components/Mask'
@@ -6,16 +7,22 @@ import Nav from './components/Nav'
 import About from './components/About'
 import SideBar from './components/SideBar'
 import Home from './components/Home'
+import styled from 'styled-components'
+import LoadingSet from './components/StorySets/LoadingSet'
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
 
 const App = () => {
+  const {loading} = useSelector(state => state.photoReducer)
   return (
-    <div>
+    <AppContainer>
       <Nav />
-      <Home />
-
-      <Mask />
-      {/* <About /> */}
-    </div>
+      <SideBar />
+      {loading ? <LoadingSet /> : <Home />}
+    </AppContainer>
   )
 }
 
