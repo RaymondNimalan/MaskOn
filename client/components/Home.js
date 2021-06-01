0fdb4b129460bfa065b88ef7c1fcc992dd8c03c4
Ray Nimalan <raymondnimalan@gmail.com>
2021-06-01 16:46:46 -0400

    feat: home component toggles btween webcam and results

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,7 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {MainContainer} from './styled/HomeStyles'
-import SideBar from './SideBar'
+import {
+  MainContainer,
+  CameraContainer,
+  ResultsContainer,
+  LoadingContainer
+} from './styled/HomeStyles'
 import WebcamCapture from './WebcamCapture'
 import LoadingSet from './StorySets/LoadingSet'
 import YesMask from './StorySets/YesMask'
@@ -14,15 +18,53 @@ const Home = () => {
 
   const {labels, loading} = useSelector(state => state.photoReducer)
 
+  const toggle = useSelector(state => state.toggleReducer)
+
+  useEffect(
+    () => {
+      console.log('toggle', toggle)
+    },
+    [toggle]
+  )
+
+  useEffect(
+    () => {
+      console.log(labels)
+      if (labels.includes('Beard')) {
+        setMask(true)
+      } else {
+        setMask(false)
+      }
+    },
+    [labels]
+  )
+
   return (
     <MainContainer>
-      <SideBar />
-      {!loading ? <WebcamCapture /> : <LoadingSet />}
-
-      {/* {mask ? <NoMask /> : <YesMask />}
-      <LoadingSet /> */}
+      {!toggle ? (
+        <CameraContainer>
+          <WebcamCapture />
+        </CameraContainer>
+      ) : (
+        <h1>RESULTS</h1>
+      )}
     </MainContainer>
   )
+
+  // if (toggle) {
+  //   return (
+  //     <MainContainer>
+  //       {loading ? <WebcamCapture /> : <LoadingSet />}
+  //       <CameraContainer></CameraContainer>
+  //     </MainContainer>
+  //   )
+  // } else {
+  //   return (
+  //     <MainContainer>
+  //       <ResultsContainer>{mask ? <YesMask /> : <NoMask />}</ResultsContainer>
+  //     </MainContainer>
+  //   )
+  // }
 }
 
 export default Home
--- client/components/styled/HomeStyles.js
+++ client/components/styled/HomeStyles.js
@@ -2,11 +2,20 @@ import styled from 'styled-components'
 
 export const MainContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  //flex-direction: row;
+`
+export const CameraContainer = styled.div`
+  display: flex;
 `
 
-// export const CameraContainer = styled.div``
-// export const CameraContainer = styled.div``
+export const LoadingContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`
+
+export const ResultsContainer = styled.div``
+
 // export const CameraContainer = styled.div``
 // export const CameraContainer = styled.div``
 // export const CameraContainer = styled.div``
