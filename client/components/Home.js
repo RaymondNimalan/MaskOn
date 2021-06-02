dbcf19f075585bee65572dc09df44bb778586e26
Ray Nimalan <raymondnimalan@gmail.com>
2021-06-02 18:53:47 -0400

    feat: home comp refactor + styling

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,43 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {
-  MainContainer,
-  CameraContainer,
-  ResultsContainer,
-  LoadingContainer
-} from './styled/HomeStyles'
+import {MainContainer, CameraContainer} from './styled/HomeStyles'
 import WebcamCapture from './WebcamCapture'
-import LoadingSet from './StorySets/LoadingSet'
-import YesMask from './StorySets/YesMask'
-import NoMask from './StorySets/NoMask'
+import Result from './Result'
 
 const Home = () => {
-  const [mask, setMask] = useState(false)
-
-  //const dispatch = useDispatch()
-
-  const {labels, loading} = useSelector(state => state.photoReducer)
-
   const toggle = useSelector(state => state.toggleReducer)
-
-  useEffect(
-    () => {
-      console.log('toggle', toggle)
-    },
-    [toggle]
-  )
-
-  useEffect(
-    () => {
-      console.log(labels)
-      if (labels.includes('Beard')) {
-        setMask(true)
-      } else {
-        setMask(false)
-      }
-    },
-    [labels]
-  )
 
   return (
     <MainContainer>
@@ -46,25 +14,10 @@ const Home = () => {
           <WebcamCapture />
         </CameraContainer>
       ) : (
-        <h1>RESULTS</h1>
+        <Result />
       )}
     </MainContainer>
   )
-
-  // if (toggle) {
-  //   return (
-  //     <MainContainer>
-  //       {loading ? <WebcamCapture /> : <LoadingSet />}
-  //       <CameraContainer></CameraContainer>
-  //     </MainContainer>
-  //   )
-  // } else {
-  //   return (
-  //     <MainContainer>
-  //       <ResultsContainer>{mask ? <YesMask /> : <NoMask />}</ResultsContainer>
-  //     </MainContainer>
-  //   )
-  // }
 }
 
 export default Home
--- client/components/styled/HomeStyles.js
+++ client/components/styled/HomeStyles.js
@@ -2,19 +2,11 @@ import styled from 'styled-components'
 
 export const MainContainer = styled.div`
   display: flex;
-  //flex-direction: row;
-`
-export const CameraContainer = styled.div`
-  display: flex;
-`
-
-export const LoadingContainer = styled.div`
-  display: flex;
+  flex-direction: row;
   width: 100%;
-  height: 100%;
+  justify-content: space-around;
 `
-
-export const ResultsContainer = styled.div``
+export const CameraContainer = styled(MainContainer)``
 
 // export const CameraContainer = styled.div``
 // export const CameraContainer = styled.div``
