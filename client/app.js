b2ffb6d28a4c8df322f0f3985eff85f77f70bdbf
Ray Nimalan <raymondnimalan@gmail.com>
2021-06-04 14:29:33 -0400

    feat: new virus particle effect for sidebar

--- client/app.js
+++ client/app.js
@@ -9,19 +9,35 @@ import SideBar from './components/SideBar'
 import Home from './components/Home'
 import styled from 'styled-components'
 import LoadingSet from './components/StorySets/LoadingSet'
+import ParticlesBg from 'particles-bg'
+import icon from '../public/images/icon'
 
 const AppContainer = styled.div`
   display: flex;
   flex-direction: row;
+  //background-color: #deeffe;
+  //z-index: -999;
 `
 const SideBarContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 350px;
+  object-fit: contain;
+  //background-color: #f6feaa;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `
 
 const MainContainer = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100%;
   justify-content: center;
+  align-items: center;
+  background-color: #f6feaa;
 `
 const LoadingContainer = styled.div`
   display: flex;
@@ -30,10 +46,38 @@ const LoadingContainer = styled.div`
 `
 const App = () => {
   const {loading} = useSelector(state => state.photoReducer)
+
+  const config = {
+    num: [1, 2],
+    rps: 0.1,
+    radius: [5, 40],
+    life: [1.5, 3],
+    v: [1, 2],
+    tha: [1, 3],
+    alpha: [0.6, 0],
+    scale: [0.1, 0.2],
+    body: icon,
+    position: 'all',
+    color: ['random', '#ff0000'],
+    cross: 'dead',
+    random: 1
+  }
+
   return (
     <AppContainer>
       <Nav />
       <SideBarContainer>
+        <ParticlesBg
+          type="custom"
+          num={1}
+          config={config}
+          bg={{
+            display: 'flex',
+            position: 'absolute',
+            zIndex: '-2',
+            width: '100%'
+          }}
+        />
         <SideBar />
       </SideBarContainer>
       <MainContainer>
