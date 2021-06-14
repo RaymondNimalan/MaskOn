816887be99fbff28e5a5fc37cdac88ea8245bc0b
Ray Nimalan <raymondnimalan@gmail.com>
2021-06-14 18:35:57 -0400

    feat: removed mask icon and replaced with app name

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -2,20 +2,16 @@ import React, {useState, useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {HiDotsVertical} from 'react-icons/hi'
 import {RiSurgicalMaskLine} from 'react-icons/ri'
-import {Navbar, Icon, NavMenu} from './styled/NavStyles'
+import {Navbar, Icon, NavMenu, Title} from './styled/NavStyles'
 import {dropToggle} from '../store/toggle'
 
 const Nav = () => {
-  const {dropdownToggle} = useSelector(state => state.toggleReducer)
-
   const dispatch = useDispatch()
 
   return (
     <Navbar>
       <NavMenu>
-        <Icon>
-          <RiSurgicalMaskLine />
-        </Icon>
+        <Title>MaskOn</Title>
         <Icon>
           <HiDotsVertical onClick={() => dispatch(dropToggle())} />
         </Icon>
