520aefbbcbeded866e2f27f7792d87a507cdc1e6
Ray Nimalan <raymondnimalan@gmail.com>
2021-06-14 18:38:12 -0400

    fix: removed unused actions

--- client/store/photo.js
+++ client/store/photo.js
@@ -1,21 +1,17 @@
 import axios from 'axios'
 import {homeToggle} from './toggle'
+import {Toast} from '../components/Toast/toast'
 
 // INITIAL STATE
 
 const intialState = {
   labels: [],
-  mask: null,
   loading: false
 }
 
 // ACTION TYPE
 
 export const ANALYSIS = 'ANALYSIS'
-
-export const MASK_TRUE = 'MASK_TRUE'
-
-export const MASK_FALSE = 'MASK_FALSE'
 
 export const LOADING = 'LOADING'
 
@@ -24,14 +20,6 @@ export const LOADING = 'LOADING'
 export const analysis = data => ({
   type: ANALYSIS,
   data
-})
-
-export const maskOn = () => ({
-  type: MASK_TRUE
-})
-
-export const maskOff = () => ({
-  type: MASK_FALSE
 })
 
 export const loading = () => ({
@@ -43,16 +31,24 @@ export const loading = () => ({
 export const _analysis = image => async dispatch => {
   try {
     const {data} = await axios.post('/api/', image)
-    dispatch(loading())
-    dispatch(analysis(data))
-    setTimeout(() => {
+    if (data.length === 1) {
+      console.log('error in image')
+      return new Toast({
+        message: 'Capture a photo with the Camera Button before uploading!',
+        type: 'danger'
+      })
+    } else {
       dispatch(loading())
-    }, 4000)
-    setTimeout(() => {
-      dispatch(homeToggle())
-    }, 4001)
-  } catch (error) {
-    console.log(error)
+      dispatch(analysis(data))
+      setTimeout(() => {
+        dispatch(loading())
+      }, 4000)
+      setTimeout(() => {
+        dispatch(homeToggle())
+      }, 4001)
+    }
+  } catch (err) {
+    console.log(err)
   }
 }
 
@@ -62,10 +58,6 @@ const photoReducer = (state = intialState, action) => {
       return {...state, labels: action.data}
     case LOADING:
       return {...state, loading: !state.loading}
-    case MASK_TRUE:
-      return {...state, mask: true}
-    case MASK_FALSE:
-      return {...state, mask: false}
     default:
       return state
   }
